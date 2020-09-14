263ebda329eedf83ebdff4623a67a6cde5408e82
DavideRagaini <DavideRagaini@gmail.com>
2020-09-14 09:09:53 +0200

    added tppctl

--- laptop.h
+++ laptop.h
@@ -29,15 +29,18 @@ typedef struct {
 	const char *name;
 	const void *cmd;
 } Sp;
+
 const char *spcmd0[] = {"st", "-n", "spterm", "-g", "120x40", NULL };
 const char *spcmd1[] = {"st", "-n", "spcalc", "-f", "monospace:size=16", "-g", "50x20", "-e", "bc", "-lq", NULL };
 const char *spcmd2[] = {"st", "-n", "spaudio", "-g", "120x20", "-e", "pulsemixer", NULL };
 const char *spcmd3[] = {"st", "-n", "spncmpcpp", "-g", "120x35", "-e", "ncmpcpp", NULL };
-const char *spcmd4[] = {"st", "-n", "spspotify", "-g", "120x50", "-e", "spotify", NULL };
+/* const char *spcmd4[] = {"st", "-n", "spspotify", "-g", "120x50", "-e", "spotify", NULL }; */
+const char *spcmd4[] = {"st", "-n", "spspotify", "-g", "140x35", "-e", "startspotify", NULL };
 const char *spcmd5[] = {"st", "-n", "splf", "-g", "120x30", "-e", "lf", NULL };
 const char *spcmd6[] = {"st", "-n", "spnews", "-g", "120x45", "-e", "newsboat", NULL };
 const char *spcmd7[] = {"st", "-n", "spgotop", "-g", "130x40", "-e", "gotop", NULL };
-/* const char *spcmd6[] = {"st", "-n", "spscratch", "-g", "125x40", "-e", "vim", "~/.config/void/vimwiki/Scratch.md", NULL }; */
+const char *spcmd8[] = {"st", "-n", "spvcs", "-g", "130x40", "-e", "vim", "$HOME/Documents/Documentations/Vim/VimCheatSheet.md", NULL };
+
 static Sp scratchpads[] = {
 	/* name          cmd  */
 	{"spterm",	spcmd0},
@@ -48,7 +51,7 @@ static Sp scratchpads[] = {
 	{"splf",	spcmd5},
 	{"spnews",	spcmd6},
 	{"spgotop",	spcmd7},
-	/* {"spscratch",	spcmd6}, */
+	{"spvcs",	spcmd8},
 };
 
 /* tagging */
@@ -68,11 +71,11 @@ static const Rule rules[] = {
 	{ NULL,      "spcalc",    NULL,       SPTAG(1),     1,           1,         0,        -1 },
 	{ NULL,      "spaudio",   NULL,       SPTAG(2),     1,           1,         0,        -1 },
 	{ NULL,      "spncmpcpp", NULL,       SPTAG(3),     1,           1,         0,        -1 },
-	{ NULL,      "spspotify", NULL,       SPTAG(4),     1,           1,         0,        -1 },
+	{ NULL,      "spspotify",  NULL,      SPTAG(4),     1,           1,         0,        -1 },
 	{ NULL,      "splf",      NULL,       SPTAG(5),     1,           1,         0,        -1 },
 	{ NULL,      "spnews",    NULL,       SPTAG(6),     1,           1,         0,        -1 },
 	{ NULL,      "spgotop",   NULL,       SPTAG(7),     1,           1,         0,        -1 },
-	/* { NULL,      "spscratch", NULL,       SPTAG(6),     1,           1,         0,        -1 }, */
+	{ NULL,      "spvcs",     NULL,       SPTAG(8),     1,           1,         0,        -1 },
 };
 
 /* layout(s) */
@@ -203,7 +206,7 @@ static Key keys[] = {
 	/* { MODKEY|ShiftMask,		XK_z,		spawn,		SHCMD("") }, */
 	{ MODKEY,			XK_x,		incrgaps,	{.i = -3 } },
 	/* { MODKEY|ShiftMask,		XK_x,		spawn,		SHCMD("") }, */
-	/* { MODKEY,			XK_c,		spawn,		SHCMD("") }, */
+	{ MODKEY,			XK_c,		togglescratch,	{.ui = 8} },
 	/* { MODKEY|ShiftMask,		XK_c,		spawn,		SHCMD("") }, */
 	/* { MODKEY,			XK_v,		spawn,		SHCMD("") }, */
 	/* { MODKEY|ShiftMask,		XK_v,		spawn,		SHCMD("") }, */
@@ -268,7 +271,7 @@ static Key keys[] = {
 	{ 0, XF86XK_AudioPrev,		spawn,		SHCMD("dmpc prev") },
 	{ 0, XF86XK_AudioNext,		spawn,		SHCMD("dmpc next") },
 	{ 0, XF86XK_AudioPlay,		spawn,		SHCMD("dmpc toggle") },
-	{ MODKEY, XF86XK_AudioPlay,	spawn,		SHCMD("pauseallmpv") },
+	{ MODKEY, XF86XK_AudioPlay,	spawn,		SHCMD("tppctl") },
 	{ 0, XF86XK_AudioStop,		spawn,		SHCMD("st -e pulsemixer; kill -44 $(pidof dwmblocks)") },
 	/* { 0, XF86XK_AudioRewind,	spawn,		SHCMD("mpc seek -10") }, */
 	/* { 0, XF86XK_AudioForward,	spawn,		SHCMD("mpc seek +10") }, */
