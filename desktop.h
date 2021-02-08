577e4d8690bfdf00ac8b511bcb6ef6adf2ac18b7
DavideRagaini <DavideRagaini@gmail.com>
2021-02-08 13:54:55 +0100

    tmux dropdowns

--- desktop.h
+++ desktop.h
@@ -5,8 +5,8 @@
 #define TERMCLASS "St"
 
 /* appearance */
-static const unsigned int borderpx  = 5;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int snap      = 10;       /* snap pixel */
 static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
 static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
@@ -16,7 +16,7 @@ static const int smartgaps          = 1;        /* 1 means no outer gap when the
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Iosevka:size=12", "Inconsolata:pixelsize=12:antialias=true:autohint=true"  };
-static char dmenufont[]             = "Iosevka:size=30";
+static char dmenufont[]             = "Iosevka:size=24";
 static char normbgcolor[]           = "#2F0B3A";
 static char normbordercolor[]       = "#BD93F9"; //707870
 static char normfgcolor[]           = "#cc00cc"; //815ba4
@@ -38,10 +38,10 @@ const char *spcmd1[] = {"st", "-n", "spcalc", "-f", "monospace:size=16", "-g", "
 const char *spcmd2[] = {"st", "-n", "spaudio", "-g", "120x20", "-e", "pulsemixer", NULL };
 const char *spcmd3[] = {"st", "-n", "spncmpcpp", "-g", "120x35", "-e", "ncmpcpp", NULL };
 const char *spcmd4[] = {"st", "-n", "spspotify-tui", "-g", "140x35", "-e", "startspotify", NULL };
-const char *spcmd5[] = {"st", "-n", "spranger", "-g", "120x30", "-e", "ranger", NULL };
+const char *spcmd5[] = {"st", "-n", "spranger", "-g", "120x30", "-e", "tmux", "new-session", "-s", "ranger", "ranger", NULL };
 const char *spcmd6[] = {"st", "-n", "spnews", "-g", "120x45", "-e", "newsboat", NULL };
 const char *spcmd7[] = {"st", "-n", "spgotop", "-g", "130x40", "-e", "gotop", NULL };
-const char *spcmd8[] = {"st", "-n", "splf", "-g", "120x30", "-e", "lf", NULL };
+const char *spcmd8[] = {"st", "-n", "splf", "-g", "120x30", "-e", "lf", "tmux", "new-session", "-s", "lf", "lf", NULL };
 
 static Sp scratchpads[] = {
 	/* name          cmd  */
