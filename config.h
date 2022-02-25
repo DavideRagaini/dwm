20a163515e57c7a5f7ae3a3ebc9bdf26dd28b55f
DavideRagaini <DavideRagaini@gmail.com>
2022-02-25 11:04:38 +0100

    Number signals

--- config.h
+++ config.h
@@ -5,17 +5,18 @@ static const unsigned int gappih    = 20;       /* horiz inner gap between windo
 static const unsigned int gappiv    = 20;       /* vert inner gap between windows */
 static const unsigned int gappoh    = 20;       /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov    = 20;       /* vert outer gap between windows and screen edge */
-static const char *fonts[]        = { "Hack Nerd Font Mono:size=11:antialias=true:autohint=true", "FiraCode Nerd Font Mono:pixelsize=10:antialias=true:autohint=true"  };
 static int smartgaps                = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
-static const char dmenufont[]     = "Hack Nerd Font Mono:size=18";
-static const char normbgcolor[]         = "#2F0B3A";
-static const char normbordercolor[]     = "#BD93F9";
-static const char normfgcolor[]         = "#cc00cc";
-static const char selfgcolor[]          = "#dddddd";
-static const char selbordercolor[]      = "#FF00FF";
-static const char selbgcolor[]          = "#571dc2";
+static const char *fonts[]        = { "Hack Nerd Font Mono:size=9:antialias=true:autohint=true",
+                                      "FiraCode Nerd Font Mono:pixelsize=9:antialias=true:autohint=true"  };
+static const char dmenufont[]       = "Hack Nerd Font Mono:size=18";
+static const char normbgcolor[]     = "#2F0B3A";
+static const char normbordercolor[] = "#BD93F9";
+static const char normfgcolor[]     = "#cc00cc";
+static const char selfgcolor[]      = "#dddddd";
+static const char selbordercolor[]  = "#FF00FF";
+static const char selbgcolor[]      = "#571dc2";
 static const char *colors[][3] = {
        /*               fg           bg           border   */
        [SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
@@ -222,13 +223,13 @@ static Key keys[] = {
 
     /* { MODKEY,              XK_F1,          spawn,           SHCMD("") }, */
     /* { MODKEY,              XK_F2,          spawn,           SHCMD("") }, */
-    { MODKEY,              XK_F3,          spawn,           SHCMD("wifi toggle && sleep 10 && pkill -RTMIN+16 dwmblocks") },
-    { MODKEY|ShiftMask,    XK_F3,          spawn,           SHCMD("bluetooth toggle && pkill -RTMIN+7 dwmblocks") },
+    { MODKEY,              XK_F3,          spawn,           SHCMD("wifi toggle && sleep 10 && pkill -RTMIN+3 dwmblocks") },
+    { MODKEY|ShiftMask,    XK_F3,          spawn,           SHCMD("bluetooth toggle && pkill -RTMIN+4 dwmblocks") },
     { MODKEY,              XK_F5,          spawn,           SHCMD("xrdb -merge ~/.config/X11/xresources_dir/light") },
     { MODKEY|ShiftMask,    XK_F5,          spawn,           SHCMD("xrdb -remove") },
     /* { MODKEY,              XK_F6,          spawn,           SHCMD("") }, */
     { MODKEY,              XK_F7,          spawn,           SHCMD("i3lock -e -f -c 1d2021 -i ~/.config/Icons/lockscreen.png; xset dpms force off") },
-    { MODKEY,              XK_F8,          spawn,           SHCMD("t-tlp") },
+    { MODKEY,              XK_F8,          spawn,           SHCMD("t-tlp && pkill -RTMIN+7 dwmblocks") },
     { MODKEY,              XK_F9,          spawn,           SHCMD("dmenumount") },
     { MODKEY,              XK_F10,         spawn,           SHCMD("dmenuumount") },
     /* { MODKEY,              XK_F11,         spawn,           SHCMD("") }, */
@@ -242,10 +243,10 @@ static Key keys[] = {
     { MODKEY,              XK_Delete,      togglescratch,   {.ui = 6} },
     /* { MODKEY,              XK_Scroll_Lock, spawn,           SHCMD("") }, */
 
-    { 0,        XF86XK_AudioMute,          spawn,           SHCMD("amixer set -q Master toggle; kill -44 $(pidof dwmblocks)") },
-    { MODKEY,   XF86XK_AudioMute,          spawn,           SHCMD("output-audio") },
-    { 0,        XF86XK_AudioRaiseVolume,   spawn,           SHCMD("pactl set-sink-volume @DEFAULT_SINK@ +5%; kill -44 $(pidof dwmblocks)") },
-    { 0,        XF86XK_AudioLowerVolume,   spawn,           SHCMD("pactl set-sink-volume @DEFAULT_SINK@ -5%; kill -44 $(pidof dwmblocks)") },
+    { 0,        XF86XK_AudioMute,          spawn,           SHCMD("amixer set -q Master toggle; kill -39 $(pidof dwmblocks)") },
+    { MODKEY,   XF86XK_AudioMute,          spawn,           SHCMD("output-audio && pkill -RTMIN+5 dwmblocks") },
+    { 0,        XF86XK_AudioRaiseVolume,   spawn,           SHCMD("pactl set-sink-volume @DEFAULT_SINK@ +5%; kill -39 $(pidof dwmblocks)") },
+    { 0,        XF86XK_AudioLowerVolume,   spawn,           SHCMD("pactl set-sink-volume @DEFAULT_SINK@ -5%; kill -39 $(pidof dwmblocks)") },
     /* { 0,        XF86XK_AudioMute,          spawn,           SHCMD("pulsemixer --toggle-mute; kill -44 $(pidof dwmblocks)") }, */
     /* { MODKEY,   XF86XK_AudioMute,          togglescratch,   {.ui = 3} }, */
     /* { 0,           XF86XK_AudioRaiseVolume,spawn,           SHCMD("pulsemixer --change-volume +5; kill -44 $(pidof dwmblocks)") }, */
@@ -265,8 +266,8 @@ static Key keys[] = {
     { 0,        XF86XK_Calculator,         togglescratch,   {.ui = 2} },
     /* { MODKEY,   XF86XK_Calculator,         spawn,           SHCMD("") }, */
     /* { 0,        XF86XK_Sleep,              spawn,           SHCMD("") }, */
-    { 0,        XF86XK_WWW,                spawn,           SHCMD("wifi toggle && sleep 10 && pkill -RTMIN+16 dwmblocks") },
-    { 0,        XF86XK_WWAN,               spawn,           SHCMD("wifi toggle && sleep 10 && pkill -RTMIN+16 dwmblocks") },
+    { 0,        XF86XK_WWW,                spawn,           SHCMD("wifi toggle && sleep 10 && pkill -RTMIN+3 dwmblocks") },
+    { 0,        XF86XK_WWAN,               spawn,           SHCMD("wifi toggle && sleep 10 && pkill -RTMIN+3 dwmblocks") },
     /* { 0,        XF86XK_DOS,                spawn,           SHCMD("") }, */
     /* { 0,        XF86XK_ScreenSaver,        spawn,           SHCMD("") }, */
     /* { 0,        XF86XK_TaskPane,           spawn,           SHCMD("") }, */
